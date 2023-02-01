5b053ad14b60a7425ba6049543378526fb088d62
gutolin <gutolin12@yandex.ru>
2023-02-02 00:03:32 +0300

    Добавление исключений workflow

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,5 +1,6 @@
-from http import HTTPStatus
 from django.test import Client, TestCase
+
+from http import HTTPStatus
 
 
 class StaticPagesURLTests(TestCase):
@@ -20,4 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+        self.assertContains(response, 'А это вторая страница')
